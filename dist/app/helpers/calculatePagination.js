9a72d9e990feb3165cbfec8c44832f5a56d59220
abdullahboshir <abdullahboshir@gmail.com>
2024-06-09 20:05:55 +0600

    deploy the project

--- dist/app/helpers/calculatePagination.js
+++ dist/app/helpers/calculatePagination.js
@@ -7,7 +7,7 @@ const calculatePagination = (paginateData) => {
     }
     ;
     const page = Number(paginateData.page) || 1;
-    const limit = Number(paginateData.limit) || 10;
+    const limit = Number(paginateData.limit) || 9;
     const skip = (page - 1) * limit;
     const sortBy = paginateData.sortBy || 'destination';
     const sortOrder = paginateData.sortOrder || 'desc';
--- src/app/helpers/calculatePagination.ts
+++ src/app/helpers/calculatePagination.ts
@@ -15,7 +15,7 @@ export const calculatePagination = (paginateData: TPaginate) => {
     };
 
     const page = Number(paginateData.page) || 1;
-    const limit = Number(paginateData.limit) || 10;
+    const limit = Number(paginateData.limit) || 9;
     const skip = (page - 1) * limit;
 
     const sortBy = paginateData.sortBy || 'destination';
